4eb3b870fe33c49045821db0b56aec637bbc208f
Elium98 <2994988240101@ingenieria.usac.edu.gt>
2020-06-09 12:40:14 -0600

    version final

--- 201709400-Cortos980/corto1/corto1.py
+++ 201709400-Cortos980/corto1/corto1.py
@@ -1,65 +1,18 @@
-'''
-def Coll(num):
-    all_seq = []
-        while (n > 1):
-            if n % 2 == 0:
-                n = n / 2
-            else:
-                n = 3 * n + 1
-            all_seq.append(n)
-            print(all_seq)
-    return lista
+def collatz(number):                        #funcion para sacar los valores
+    lista=[]
+    while number != 1:                      #Se inicia la cuenta
+        if number % 2 == 0:                 #se especifica las condiciones
+            number = number // 2
+            print(number)
+            lista.append(number)
 
+        elif number % 2 == 1:
+            number = number * 3 + 1
+            print(number)
+            lista.append(number)
 
-for n in range(2,400):
-    all_seq = []
-    if n % 2 == 0:
-        n = n / 2
-    else:
-        n = 3 * n + 1
-    all_seq.append(n)
-    print(all_seq)
-
-
- while numero != 1:
-        if numero % 2 == 0:
-        print("%d," % (numero), end = "")
-        numero = numero / 2
-    else:
-        print("%d," % (numero), end = "")
-        numero = (numero * 3) + 1
-
-    if numero == 1:
-        print("1")
-
-for i in range(2,400):
-    total=[]
-    numero=i
-    while numero != 1:
-        lista=[]
-        if numero % 2 == 0:
-            numero = numero / 2
-            lista.append(numero)
-        else:
-            numero = (numero * 3) + 1
-            lista.append(numero)
-        if numero == 1:
-            lista.append(numero)
-        total.append(lista)
-        '''
-
-
-def sist(num):
-    lista=[]     
-    if n % 2 == 0:
-        n = n / 2
-    else:
-        n = 3 * n + 1
-    lista.append(n) 
-    return lista
-
-for i in range(2,400):
-    n=i
-    while(n>1):
-        sist(n)
-    total.append(lista)
+try:
+    num = int(input("Please pick any whole number to see the Collatz Sequence in action.n"))
+    collatz(num)                                #Se implementa la funcion
+except ValueError:
+    print("Please use whole numbers only.")
