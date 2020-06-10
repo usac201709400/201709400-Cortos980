fcbf0c666522325c5a2fe2d9ae7d06d02f4a7f00
Ottoniel Sierra <ottonielsierrama@gmail.com>
2020-06-10 15:46:53 -0600

    corto calificado

--- 201709400-Cortos980/corto1/corto1.py
+++ 201709400-Cortos980/corto1/corto1.py
@@ -1,6 +1,6 @@
 def collatz(number):                        #funcion para sacar los valores
-    lista=[]
-    while number != 1:                      #Se inicia la cuenta
+    lista=[]# no realiza nada en todo el codigo
+    while number != 1:                      #Se inicia la cuenta, cuenta?
         if number % 2 == 0:                 #se especifica las condiciones
             number = number // 2
             print(number)
@@ -16,3 +16,15 @@ try:
     collatz(num)                                #Se implementa la funcion
 except ValueError:
     print("Please use whole numbers only.")
+
+#que casualidad se ve muy parecido al codigo dentro de esta pagina
+#https://living-sun.com/es/python/686209-collatz-sequence-python-sequence-collatz.html
+
+#Funcionamiento:        0/40
+#Uso de Funciones       0/20
+#Manejo de archivos     0/10
+#Manejo de Listas       0/10
+#Uso de git             20/20
+#*****************************
+#Total                  20/100
+
